e30dd249606c716bc0a2904d520626596378a4f9
Aaron Dewes <aaron.dewes@web.de>
2021-02-17 09:12:08 +0100

    Fix

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -1,4 +1,4 @@
-import { Editor } from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
 import checkMobile from 'ismobilejs';
 import { useState, useEffect } from 'react';
 import MobileEditor from './MobileEditor';
